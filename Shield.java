d0979fff6edf607e38c77d00ba8c9c86d6815519
Jean-christophe Rocher <jcrocher@orange.fr>
2020-01-13 11:43:35 +0100

    commit result

--- Shield.java
+++ Shield.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Shield {
 
@@ -13,10 +14,14 @@ public class Shield {
         names.add("Jemma Simmons");
 
         // TODO 1 : map names to agents list
-        List<Agent> agents = new ArrayList<>();
-
+        List<Agent> agents = names.stream()
+                .map(item -> new Agent(item.split(" ")[0], item.split(" ")[1]))
+                .collect(Collectors.toList());
+        
         showAgents(agents);
     }
+        
+        
 
     // don't touch anything here !
     private static void showAgents(List<Agent> agents) {
